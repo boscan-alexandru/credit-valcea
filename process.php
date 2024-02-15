c2e0f08eb8df2e4f1098b424e8f12d5f5d352ec4
boscan-alexandru <alexboscan4@gmail.com>
2024-02-15 08:49:28 +0200

    ✨ New: Added node modules

--- process.php
+++ process.php
@@ -6,13 +6,18 @@
     $name = $_REQUEST['name'];
     $headers = "From: $from";
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"subject"} = "subject";
-    $fields{"message"} = "message";
+    $fields = [
+        "name" => "",
+        "email" => "",
+        "subject" => "",
+        "message" => "",
+    ];
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $body = "Here is what was sent:\n\n";
+
+    foreach($fields as $a => $b){
+        $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]);
+    }
 
     $send = mail($to, $subject, $body, $headers);
 
